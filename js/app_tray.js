b8ab0cf8857e489119969de042632f303eca9a0c
DBKynd <db@dbkynd.com>
2017-01-08 18:24:29 -0700

    Refactor to app_tray.js

--- js/app_tray.js
+++ js/app_tray.js
@@ -1,33 +1,37 @@
 'use strict';
-var tray = new Tray(path.join(__dirname, 'images/icon.ico'));
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
+/* eslint no-console: 0 */
 
-var contextMenu = Menu.buildFromTemplate([
-    {
-        label: 'Restore',
-        click: () => {
-            ipc.send('restore_main'); //Restore Main window
-        }
+const tray = new Tray(path.join(__dirname, 'images/icon.ico'));
+
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: 'Restore',
+    click: () => {
+      ipc.send('restore_main'); // Restore Main window
     },
-    {
-        label: 'Reset Position',
-        click: () => {
-            ipc.send('reset_position'); //Restore Main window
-        }
+  },
+  {
+    label: 'Reset Position',
+    click: () => {
+      ipc.send('reset_position'); // Restore Main window
     },
-    {
-        type: 'separator'
+  },
+  {
+    type: 'separator',
+  },
+  {
+    label: 'Exit',
+    click: () => { // Force quit app, ignores close to tray setting
+      ipc.send('force_quit');
     },
-    {
-        label: 'Exit',
-        click: () => { //Force quit app, ignores close to tray setting
-            ipc.send('force_quit');
-        }
-    }
+  },
 ]);
 
 tray.setToolTip('ControlCast');
 tray.setContextMenu(contextMenu);
 
-tray.on('double-click', function () {
-    ipc.send('toggle_minimize'); //Toggle the minimize state of the app on double click on tray icon
+tray.on('double-click', () => {
+  ipc.send('toggle_minimize'); // Toggle the minimize state of the app on double click on tray icon
 });
